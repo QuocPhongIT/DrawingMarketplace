b8e9fc4da4b43047a61e67c3dec36d3c045e4ccc
unknown <phongindra@gmail.com>
2025-12-29 00:53:02 +0700

    Fix DATABASE_CONNECTION loading from environment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.HttpOverrides;
 using DotNetEnv;
 
 Env.Load();
-
+AppContext.SetSwitch("Npgsql.EnableLegacyNamingConvention", false);
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
